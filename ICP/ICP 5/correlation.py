9f9e2437cd1f06c227b1c36d02c7ffa96b74b044
PranoopMutha <pranoopumkc@gmail.com>
2019-03-02 04:22:01 -0600

    ICP5 Code and images comitted
    commit

---
+++ ICP/ICP 5/correlation.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+
+
+
+#loading dataset
+dataset = pd.read_csv('train.csv')
+dataset['Sex'] = dataset['Sex'].map({'female':1, 'male':0}).astype(int)
+
+#grouping by sex
+print(dataset[['Sex', 'Survived']].groupby(['Sex'], as_index=True).mean().sort_values(by='Survived', ascending=False))
