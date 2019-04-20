2ac0a1b1ef207fbe3d07068350a10e4f10e0330f
PranoopMutha <pranoopumkc@gmail.com>
2019-04-20 06:01:48 -0500

    push screenshots
    commit

---
+++ ICP/Deep Learning ICP 4/source/test.py
@@ -0,0 +1,99 @@
+from __future__ import print_function
+import keras
+from keras.utils import to_categorical
+from keras.datasets import mnist
+from keras.models import Sequential,load_model
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
+import numpy as np
+import matplotlib.pyplot as plt
+
+batch_size = 128
+num_classes = 10
+epochs = 2
+
+# input image dimensions
+img_rows, img_cols = 28, 28
+
+# the data, split between train and test sets
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+if K.image_data_format() == 'channels_first':
+    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
+    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
+    input_shape = (1, img_rows, img_cols)
+else:
+    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+    input_shape = (img_rows, img_cols, 1)
+#
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+
+train_labels_one_hot = to_categorical(x_train)
+test_labels_one_hot = to_categorical(x_test)
+
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+#
+# # convert class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3),activation='relu',input_shape=input_shape))
+model.add(Dropout(0.2))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(Dropout(0.2))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(Dropout(0.2))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Flatten())
+model.add(Dropout(0.2))
+model.add(Dense(1024, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(512, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(num_classes, activation='softmax'))
+
+tbCallBack= keras.callbacks.TensorBoard(log_dir='./Graph', write_images=True)
+
+
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adadelta(),
+              metrics=['accuracy'])
+model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=1,
+          validation_data=(x_test, y_test), callbacks=[tbCallBack])
+
+model.save('mnist.h5')
+
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+
+
+
+model = load_model('mnist.h5')
+plt.imshow(x_test[4455].reshape(28, 28),cmap='Greys')
+print(y_test[4455].argmax())
+plt.show()
+pred = model.predict(x_test[4455].reshape(1, img_rows, img_cols, 1))
+plt.imshow(x_test[1].reshape(28, 28),cmap='Greys')
+print(y_test[1].argmax())
+plt.show()
+plt.imshow(x_test[2].reshape(28, 28),cmap='Greys')
+print(y_test[2].argmax())
+plt.show()
+plt.imshow(x_test[3].reshape(28, 28),cmap='Greys')
+print(y_test[3].argmax())
+plt.show()
+
